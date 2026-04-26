4e7ad103d2cfa44de7391e7f2e3fda9662044c8a
Islingr0822 <tristansands.work@gmail.com>
2026-04-25 21:33:05 -0600

    Preps 1 and 2
    Built C# programs that collected and output the name of the user and calculated the users letter grade based on percentage respectively.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage in the class? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string letterGrade;
+        
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        
+        else 
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+
+        if (letterGrade == "A" || letterGrade == "B" || letterGrade == "C")
+        {
+            Console.WriteLine("Congratulations on passing!");
+        }
+
+        else
+        {
+            Console.WriteLine("Tough luck kid, you'll gettem next time.");
+        }
     }
 }
