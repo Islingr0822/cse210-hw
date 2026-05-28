35237f4c4f139261e23c96c55f6aefe74a85adf5
Islingr0822 <tristansands.work@gmail.com>
2026-05-28 17:03:51 -0600

    class outlines

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,26 @@
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private string _verse;
+
+    public void SetBook()
+    {
+        
+    }
+    
+    public void SetChapter()
+    {
+        
+    }
+
+    public void SetStartVerse()
+    {
+        
+    }
+
+    public void SetEndVerse()
+    {
+        
+    }
+}
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,10 @@
+class Scripture
+{
+    private string _reference;
+    private string _word;
+
+    public void DisplayScripture()
+    {
+        
+    }
+}
