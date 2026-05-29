7430aa20f1904f4c876c04e2706e73ba89c51441
Islingr0822 <tristansands.work@gmail.com>
2026-05-29 14:59:21 -0600

    Scripture memorizing program complete

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,10 +1,53 @@
-class Scripture
+// Scripture.cs
+public class Scripture
 {
-    private string _reference;
-    private string _word;
+    private Reference _reference;
+    private List<Word> _words;
 
-    public void DisplayScripture()
+    public Scripture(string referenceText, string text)
     {
+        _reference = new Reference("John", 3, 16);
         
+        _words = new List<Word>();
+        string[] wordArray = text.Split(' ');
+        foreach (string word in wordArray)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(_reference.ToString());
+        Console.WriteLine();
+        
+        foreach (Word word in _words)
+        {
+            Console.Write(word.GetDisplayText() + " "); 
+        }
+        Console.WriteLine();
+    }
+
+    public void HideRandomWords(int count = 3)
+    {
+        Random random = new Random();
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+        
+        if (visibleWords.Count == 0)
+            return;
+            
+        int wordsToHide = Math.Min(count, visibleWords.Count);
+        
+        for (int i = 0; i < wordsToHide; i++)
+        {
+            int randomIndex = random.Next(visibleWords.Count);
+            visibleWords[randomIndex].Hide();
+            visibleWords.RemoveAt(randomIndex);
+        }
+    }
+
+    public bool AreAllWordsHidden()
+    {
+        return _words.All(w => w.IsHidden());
     }
 }
