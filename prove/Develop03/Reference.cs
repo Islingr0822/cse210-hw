5ef981f2ce9140b3724af45239e37d380b9308b2
Islingr0822 <tristansands.work@gmail.com>
2026-05-29 13:09:44 -0600

    references work

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -2,25 +2,31 @@ class Reference
 {
     private string _book;
     private int _chapter;
-    private string _verse;
+    private int _startVerse;
+    private int _endVerse;
 
     public void SetBook()
     {
-        
+        Console.WriteLine("Please enter the book the scripture is in: ");
+        _book = Console.ReadLine();
     }
     
     public void SetChapter()
     {
-        
+        Console.WriteLine("Please enter the chapter number for the scripture: ");
+        _chapter = int.Parse(Console.ReadLine());
     }
 
     public void SetStartVerse()
     {
-        
+        Console.WriteLine("Please enter start verse: ");
+        _startVerse = int.Parse(Console.ReadLine());
+
     }
 
     public void SetEndVerse()
     {
-        
+        Console.WriteLine("Please enter the end verse: ");
+        _endVerse = int.Parse(Console.ReadLine());
     }
 }
