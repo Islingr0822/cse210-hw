971b6e4bfcf778de6696337a4e3cc86903238896
Islingr0822 <tristansands.work@gmail.com>
2026-06-01 18:32:37 -0600

    inheritance in class follow along

---
+++ sandbox/Person/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Person myPerson = new Person("Bob", "Roberts", 57, 198);
+        Console.WriteLine(myPerson.GetPersonInformation());
+
+        Police myPoliceMan = new Police("Gun", "Robert", "Bob", 50, 170);
+        Console.WriteLine(myPoliceMan.GetPersonInformation());
+
+        Console.WriteLine(myPoliceMan.GetpoliceManInformation());
+    }
+
+    
+}
