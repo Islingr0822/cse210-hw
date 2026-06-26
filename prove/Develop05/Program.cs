502772746d82884afe6ef924991f4e0a4afd3c66
Islingr0822 <tristansands.work@gmail.com>
2026-06-25 20:42:21 -0600

    Menu build out

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,23 +4,90 @@ class Program
 {
     static void Main(string[] args)
     {
-        // BaseGoal myGoal = new BaseGoal();
-        // myGoal.SetName();
-        // myGoal.SetDescription();
-        // myGoal.NumberOfPoints();
-        // Console.WriteLine(myGoal.GetDisplayString());
-        // myGoal.MarkComplete();
-        // Console.WriteLine(myGoal.GetDisplayString());
+        Console.WriteLine("Welcome to the Goal Program! ");
+        Console.WriteLine("");
+        string input = "0";
+        
+        while (input != "q")
+        {
+            Console.Clear();
 
-        SimpleGoal myGoal = new SimpleGoal();
-        myGoal.CreateGoal();
-        Console.WriteLine(myGoal.GetDisplayString());
-        myGoal.RecordEvent();
-        Console.WriteLine(myGoal.GetDisplayString());
+            Console.WriteLine("Please select one of the following: ");
+            Console.WriteLine("");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("q. Quit Program");
 
-        EternalGoal myGoal2 = new EternalGoal();
-        myGoal2.CreateGoal();
-        Console.WriteLine(myGoal2.GetDisplayString());
+            Console.Write("Input: ");
+            input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                Console.WriteLine("What type of goal? ");
+                Console.WriteLine("");
+                Console.WriteLine("1. Simple Goal");
+                Console.WriteLine("2. Eternal Goal");
+                Console.WriteLine("3. Checklist Goal");
+                
+                string goalInput = "q";
+
+                if (goalInput == "1")
+                {
+                    SimpleGoal myGoal = new SimpleGoal();
+                    myGoal.CreateGoal();
+                }
+
+                else if (goalInput == "2")
+                {
+                    EternalGoal myGoal = new EternalGoal();
+                    myGoal.CreateGoal();
+                }
+
+                else if (goalInput == "3")
+                {
+                    ChecklistGoal myGoal = new ChecklistGoal();
+                    myGoal.CreateGoal();
+                }
+  
+            }
+
+            else if (input == "2")
+            {
+                
+            }
+            
+            else if (input == "3")
+            {
+                
+            }
+
+            else if (input == "4")
+            {
+                
+            }
+
+            else if (input == "5")
+            {
+                
+            }
+
+            else if (input == "q")
+            {
+             
+            }
+
+            else
+            {
+                Console.WriteLine("Incorrect value, please try again");
+            }
+            
+            
+        }
+
+        
 
     }
 }
